928c6d61f26ea8c0451f453086eecc4801a3f6de
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-09-24 16:15:22 +0530

    Update Nested_Loop.cpp

--- Nested_Loop.cpp
+++ Nested_Loop.cpp
@@ -15,14 +15,16 @@ main()
 
     for (int i = 1; i <= 15; i++) // i = 2
     {
-        if (i==5)
+      /*  if (i==5)
              continue; // will remove the 5th row and everything will be executed
+      */
         
         for (int j = 1; j <= 12; j++) // j = 1
         {
-            if (j==5)
+       /*     if (j==5)
                 break; // will remove the 5th coloumn
-            cout.width(4);
+        */
+        cout.width(4);
             cout << i * j;
         }
 
