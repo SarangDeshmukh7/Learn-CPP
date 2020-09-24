0672481d4c22115a3afe1379603bd6c921094a15
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-09-24 15:29:14 +0530

    Add files via upload

---
+++ Untitled1[1].cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+/* LOOPS */
+main()
+{
+    /*
+        for (initilization; condition; inc/dec)
+            instruction-to-repeat;
+    */
+
+   /* for (int i = 0; i < 3; i++)
+        cout << i << endl;
+
+    cout << "lala" << endl;
+*/
+/*
+    int array[100];
+
+    for (int i = 0; i < 100; i++)
+    {
+        array[i] = i;
+        cout <<  array[i] << endl;
+    }
+*/
+
+
+
+
+}
+
