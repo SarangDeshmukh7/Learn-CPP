d9e35eebd0d8e8b7a7e2fcfb2b62cb6d5a43ab4a
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-09-24 15:47:01 +0530

    Add files via upload

---
+++ Untitled1[1].cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    const int SIZEOFARRAY = 10;
+    int i = 0;
+    int array[SIZEOFARRAY];
+/*
+    while(i < SIZEOFARRAY) //i = 0
+    {
+        array[i] = 10 * i;
+        cout << array[i++] << endl;
+    }
+    */
+    while (i)
+      cout <<"lalala";
+/*
+    do
+    {
+        cout << "lala";
+
+    } while(i);
+*/
+}
