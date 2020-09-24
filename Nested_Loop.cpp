b82d28245fbc7ea5e975293a21bfa1acb4dd91cd
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-09-24 16:10:11 +0530

    Update and rename Untitled1[1].cpp to Nested_Loop.cpp

--- Nested_Loop.cpp
+++ Nested_Loop.cpp
@@ -5,20 +5,6 @@ using namespace std;
 
 main()
 {
-   /* int nr = 1234; // 4 digits
-    int nrOfDigits = 1;
-
-    cout << 1234 / 10 << endl;
-    cout << 123 / 10 << endl;
-    cout << 12 / 10 << endl;
-    cout << 1 / 10 << endl;
-
-    int tmp = nr;
-    while (tmp /= 10) //tmp = 0, nrOfDigits = 4
-        nrOfDigits++;
-
-    cout << "the number " << nr << " has " << nrOfDigits << " digits" << endl;
-*/
     /*
         1 2 3 4 5  6  7  8  9  10
         2 4 6 8 10 12 14 16 18 20
