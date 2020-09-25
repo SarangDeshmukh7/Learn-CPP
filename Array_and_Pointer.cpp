6856616c6c4837003dcec6ae1e3784aa106ecf38
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-09-25 16:13:05 +0530

    Update Array_and_Pointer.cpp

--- Array_and_Pointer.cpp
+++ Array_and_Pointer.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
-void main()
+#include <iostream>
+
+using namespace std;
+main()
 {
 int a[5] = {10,20,30,40,50};
-int *ptr = &a[0] ;// or * ptr = a;
+int *ptr = &a[0]; // or * ptr = a;
 
-cout<<*ptr<<endl;
+cout << *ptr <<endl;
 ptr = ptr + 2;
-cout <<*ptr << endl; 
+cout << *ptr << endl;
 }
