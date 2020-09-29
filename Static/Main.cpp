87b502e624e0c68bbb3b220256966dcb6cc33ae7
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-09-29 18:33:54 +0530

    Create Main.cpp

---
+++ Static/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "user.h"
+
+using namespace std;
+/*static properties and static methods */
+
+int main()
+{
+
+
+    cout << User::getCounter() << endl;
+
+    return 0;
+}
