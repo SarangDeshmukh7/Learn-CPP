1e38144a3189db5e200a58e4b5f85531ebabe04f
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-09-29 18:16:52 +0530

    Update Data_Structure.cpp

--- Data_Structure.cpp
+++ Data_Structure.cpp
@@ -9,40 +9,22 @@ struct personalData
     string surname;
     string telephoneNumber;
     short age;
-}a,b;
-void test(personalData *);
+};
 int main()
 {
-    personalData person[5];
-    a.age =50;
+    personalData person;
+   
 
-   // cout << a.age << endl;
-    person[0].name = "Arkadiusz";
-    person[1].name = "Wiola";
-    person[0].surname = "Wlodarczyk";
-    person[0].telephoneNumber = "606102340";
-    person[0].age = 22;
-/*
-    cout << person[0].name << endl;
-    cout << person[0].surname << endl;
-    cout << person[0].telephoneNumber << endl;
-    cout << person[0].age << endl;
-*/
-/*
-    cout << (*person).name << endl;
-    cout << (*&person[0]).name << endl;
-    cout << (person+1)->name << endl;
-*/
-    personalData *p = person;
+    person.name = "Arkadiusz";
+    person.name = "Wiola";
+    person.surname = "Wlodarczyk";
+    person.telephoneNumber = "606102340";
+    person.age = 22;
 
-    cout << p->name << endl;
+    cout << person.name << endl;
+    cout << person.surname << endl;
+    cout << person.telephoneNumber << endl;
+    cout << person.age << endl;
 
-    test(p); // test(person);
 
-    cout << p->name << endl;
-    return 0;
-}
-void test(personalData *person)
-{
-    person->name = "Agnes";
 }
